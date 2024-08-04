77e0f84e49c1a50689bdccbcfdac7c87b3e84c54
Joaquin Torres <joaquinmercadotorres@gmail.com>
2024-08-04 17:01:39 -0400

    Added incorrect and miss rate data + error bars

--- aggregateAnalysis.py
+++ aggregateAnalysis.py
@@ -37,6 +37,8 @@ saveLoc = dirname(dirname(fileLoc)) # Files are in a folder named 'data', and we
 trials = np.arange(1, 36) # Set an array of trial numbers
 aggDF = pd.DataFrame(index=trials) # Set df with index as trial numbers for aggregated average RTs per trial
 srDF = pd.DataFrame(index=trials) # Set df with index as trial numbers for aggregated average success rates per trial
+aggMiss = pd.DataFrame(index=trials) # Set df with index as trial numbers for aggregated miss rates (RT >= 1000) per trial
+aggInc = pd.DataFrame(index=trials) # Set df with index as trial numbers for aggregated incorrect rates (wrong key press within time limit) per trial
 surveyDF = pd.DataFrame() # Set empty df for aggregated survey Data
 
 ###################
@@ -93,10 +95,13 @@ for file in dirList:
                 transList.append([np.nan]) # instead, append a NaN value
                 count += 1
                 
-        ## 4. Reaction Time (rt)
+        ## 4. Accuracy (corr)
+        corr = np.array(exData['correct']) # create array using correct data
+        
+        ## 5. Reaction Time (rt)
         rt = np.array(exData['response_time']) # create array using response_time data
         
-        ## 5. Phase (phases)
+        ## 6. Phase (phases)
         ### Split trialNum into two arrays
         train1Trials = [x for x in trialNum if 1 <= x <= 15] # array for the first block of training trials (trials 1-15)
         train2Trials = [y for y in trialNum if 16 <= y <= 30] # array for second block of training trials (trials 16-30)
@@ -106,7 +111,7 @@ for file in dirList:
         phases = trainPhase + testPhase # Merge arrays into one to be appended into the big dataFrame
         
         # Concatenate all arrays into one big dataframe
-        allData = pd.DataFrame({'Key Press #':keyPress, 'Trial #':trialNum, 'Key Transition':transList, 'Response Time':rt, 'Phase':phases})
+        allData = pd.DataFrame({'Key Press #':keyPress, 'Trial #':trialNum, 'Key Transition':transList, 'Accuracy':corr,'Response Time':rt, 'Phase':phases})
         # Saving data
         try:
             allData.to_csv(saveDir + '/allData.csv', index=False)
@@ -142,7 +147,7 @@ for file in dirList:
         aveRTList = [average(j) for j in rtPerTrial] # make array of average RTs of each trial
         aggDF[fileName] = aveRTList # Append aveRTList to aggDF as a column    
         aveRTDF = pd.DataFrame({'Trial Number':np.arange(1,len(aveRTList)+1), 'Average RT':aveRTList}) # creates a dataframe
-        plt.plot(np.arange(1, len(aveRTList)+1), aveRTList)
+        plt.plot(trials, aveRTList)
         plt.xlabel('Trial Number')
         plt.xticks(np.arange(1, len(aveRTList)+1))
         plt.ylabel('Reaction time (ms)')
@@ -207,14 +212,12 @@ for file in dirList:
         arrayCorrect = exData['correct'].to_numpy() # isolates correct column into array
         ### NOTE: correct column encodes either 1 (correct response) or 0 (incorrect response)
         ### an average of 1 = perfect responses (no errors)
-        perTrial = list(divide_chunks(arrayCorrect, 12)) # use divide_chunks function to divide arrayCorrect into chunks of size 12
-        aveCorr = [average(i) for i in perTrial] # calculate average of each chunk
+        perTrial1 = list(divide_chunks(arrayCorrect, 12)) # use divide_chunks function to divide arrayCorrect into chunks of size 12
+        aveCorr = [average(m) for m in perTrial1] # calculate average of each chunk
         srDF[fileName] = aveCorr # Append aveCorr to srDF as a column
-        ### Create dataframe showing average success rate for each trial sequence
-        successRates = pd.DataFrame({'Trial #':trials, 'Success Rate':aveCorr})
         ### Plot success rate
         plt.figure() # reset figure
-        plt.plot(successRates['Trial #'], successRates['Success Rate'])
+        plt.plot(trials, aveCorr)
         plt.xlabel('Trial Number')
         plt.xticks(trials)
         plt.ylabel('Success Rate')
@@ -228,6 +231,57 @@ for file in dirList:
             figSR.savefig(saveDir + '/successRate.png', bbox_inches='tight')
         except FileExistsError:
             print('File already exists')
+            
+        ## 4. Incorrect rate (wrong key press within time limit of 1000 ms) within each sequence across the session
+        corrDF = pd.DataFrame({'Acc':corr, 'RT':rt}) # create df with necessary info
+        corrArr = [] # set empty array where values will be either corr (correct and w/in time limit), inc (incorrect and w/in time limit), or miss (exceeds time limit)
+        ### Assign corr, inc, or miss to each index
+        for index, row in corrDF.iterrows():
+            if corrDF['Acc'][index] == 1 and corrDF['RT'][index] < 1000:
+                corrArr.append('corr')
+            if corrDF['Acc'][index] == 0 and corrDF['RT'][index] < 1000:
+                corrArr.append('inc')
+            if corrDF['Acc'][index] == 0 and corrDF['RT'][index] >= 1000:
+                corrArr.append('miss')
+        perTrial2 = list(divide_chunks(corrArr, 12)) # use divide_chunks function to divide corrArr into chunks of size 12
+        incRatePerTrial = [n.count('inc')/12 for n in perTrial2] # Create an array that takes each sequence from perTrial2 and calculates the inc rate per trial
+        aggInc[fileName] = incRatePerTrial # Append incRatePerTrial to aggInc as a column
+        ### Plot incorrect rate
+        plt.figure() # reset figure
+        plt.plot(trials, incRatePerTrial)
+        plt.xlabel('Trial Number')
+        plt.xticks(trials)
+        plt.ylabel('Incorrect Rate')
+        plt.yticks([0, 0.5, 1.0])
+        plt.title(f'Average incorrect rates for each trial of {fileName}')
+        figIR = plt.gcf()
+        plt.show(block=False)
+        plt.pause(2)
+        plt.close()
+        try:
+            figIR.savefig(saveDir + '/incorrectRate.png', bbox_inches='tight')
+        except FileExistsError:
+            print('File already exists')
+        
+        ## 5. Miss rate (rt exceeds time limit of 1000 ms) within each sequence across the session
+        missRatePerTrial = [o.count('miss')/12 for o in perTrial2] # Create an array calculating miss rate for each sequence in perTrial2
+        aggMiss[fileName] = missRatePerTrial # Append missRatePerTrial to aggMiss as a column
+        ### Plot miss rate
+        plt.figure() # reset figure
+        plt.plot(trials, missRatePerTrial)
+        plt.xlabel('Trial Number')
+        plt.xticks(trials)
+        plt.ylabel('Miss Rate')
+        plt.yticks([0, 0.5, 1.0])
+        plt.title(f'Average miss rates for each trial of {fileName}')
+        figMR = plt.gcf()
+        plt.show(block=False)
+        plt.pause(2)
+        plt.close()
+        try:
+            figMR.savefig(saveDir + '/missRate.png', bbox_inches='tight')
+        except FileExistsError:
+            print('File already exists')        
 
 ###################
 
@@ -240,9 +294,11 @@ except FileExistsError:
 
 # Plot aggregate data
 ## Plot and save aggregate average RTs
-aggDF[aggDF >= 750] = np.nan # convert all rts >= 750 to nan (since those are the misses - late responses)
+aggDF[aggDF >= 1000] = np.nan # convert all rts >= 750 to nan (since those are the misses - late responses)
 aggDF['Mean'] = aggDF.mean(axis=1) # Create column taking the mean of each row (trial)
+aggDF['SEM'] = aggDF.iloc[:, :-1].sem(axis=1) # Create a column calculating the SEM of each row, not including the Means column
 plt.plot(trials, aggDF['Mean'])
+plt.errorbar(trials, aggDF['Mean'], yerr=aggDF['SEM'], fmt='.r', ecolor='red', elinewidth=0.5)
 plt.xlabel('Trial Number')
 plt.xticks(trials)
 plt.ylabel('Average reaction time (ms)')
@@ -255,9 +311,12 @@ try:
     figAve.savefig(saveLoc + '/aggRTs.png', bbox_inches='tight')
 except FileExistsError:
     print('File already exists')
+    
 ## Plot and save aggregate success rates
 srDF['Mean'] = srDF.mean(axis=1) # Create column taking the mean of each row (trial)
+srDF['SEM'] = srDF.iloc[:, :-1].sem(axis=1) # Create a column calculating the SEM of each row, not including the Means column
 plt.plot(trials, srDF['Mean'])
+plt.errorbar(trials, srDF['Mean'], yerr=srDF['SEM'], fmt='.r', ecolor='red', elinewidth=0.5)
 plt.xlabel('Trial Number')
 plt.xticks(trials)
 plt.ylabel('Average success rate')
@@ -269,5 +328,43 @@ plt.pause(2)
 plt.close()
 try:
     figAggSR.savefig(saveLoc + '/aggSRs.png', bbox_inches='tight')
+except FileExistsError:
+    print('File already exists')
+
+## Plot and save aggregate incorrect rates
+aggInc['Mean'] = aggInc.mean(axis=1) # Create column taking the mean of each row (trial)
+aggInc['SEM'] = aggInc.iloc[:, :-1].sem(axis=1) # Create a column calculating the SEM of each row, not including the Means column
+plt.plot(trials, aggInc['Mean'])
+plt.errorbar(trials, aggInc['Mean'], yerr=aggInc['SEM'], fmt='.r', ecolor='red', elinewidth=0.5)
+plt.xlabel('Trial Number')
+plt.xticks(trials)
+plt.ylabel('Average incorrect rate')
+plt.yticks([0, 0.5, 1.0])
+plt.title('Average incorrect rates for each trial across all participants')
+figAggIR = plt.gcf()
+plt.show(block=False)
+plt.pause(2)
+plt.close()
+try:
+    figAggIR.savefig(saveLoc + '/aggIRs.png', bbox_inches='tight')
+except FileExistsError:
+    print('File already exists')
+    
+## Plot and save aggregate miss rates
+aggMiss['Mean'] = aggMiss.mean(axis=1) # Create column taking the mean of each row (trial)
+aggMiss['SEM'] = aggMiss.iloc[:, :-1].sem(axis=1) # Create a column calculating the SEM of each row, not including the Means column
+plt.plot(trials, aggMiss['Mean'])
+plt.errorbar(trials, aggMiss['Mean'], yerr=aggMiss['SEM'], fmt='.r', ecolor='red', elinewidth=0.5)
+plt.xlabel('Trial Number')
+plt.xticks(trials)
+plt.ylabel('Average miss rate')
+plt.yticks([0, 0.5, 1.0])
+plt.title('Average miss rates for each trial across all participants')
+figAggMR = plt.gcf()
+plt.show(block=False)
+plt.pause(2)
+plt.close()
+try:
+    figAggMR.savefig(saveLoc + '/aggMRs.png', bbox_inches='tight')
 except FileExistsError:
     print('File already exists')
